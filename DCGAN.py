9f0ed791d58c0379ca06ef5db47b2169d44aceb8
RohitDulam <dulam.rohit99@gmail.com>
2018-01-19 21:55:37 +0530

    Code Cleaning

--- DCGAN.py
+++ DCGAN.py
@@ -39,19 +39,16 @@ def batch_norm(tnsr):
 
 def discriminator(x):
 
-	'''if reuse:
-		tf.get_variable_scope().reuse_variables()
-	'''
 	l1 = tf.nn.relu(batch_norm(tf.nn.conv2d(x , filter1 , strides = stride , padding = 'SAME')))
-	#print('l1' + str(l1.get_shape().as_list()))
+	
 	l2 = tf.nn.relu(batch_norm(tf.nn.conv2d(l1 , filter2 , strides = stride , padding = 'SAME')))
-	#print('l2' + str(l2.get_shape().as_list()))
+	
 	l3 = tf.nn.relu(batch_norm(tf.nn.conv2d(l2 , filter3 , strides = stride , padding = 'SAME')))
-	#print('l3' + str(l3.get_shape().as_list()))
+	
 	l4_shaping = tf.reshape(l3 , [-1 , 4 * 4 * 128])
-	#print('l4_shaping' + str(l4_shaping.get_shape().as_list()))
+	
 	l4 = tf.nn.relu(tf.matmul(l4_shaping , filter4))
-	#print('l4' + str(l4.get_shape().as_list()))
+	
 	return tf.nn.sigmoid(l4) , l4
 
 
@@ -69,23 +66,23 @@ def generator(z):
 	l1 = tf.add(tf.matmul(z , weight) , bias)
 
 	l2_shaping = tf.reshape(l1 , [batch , 4 , 4 , 32]) 
-	#print('l2_shaping' + str(l2_shaping.get_shape().as_list()))
+	
 	l2_deconv = tf.nn.conv2d_transpose(l2_shaping , filter_1 ,output_shape = [batch , 7 , 7 , 16] ,strides = [1 , 2, 2, 1], padding = 'SAME')
-	#print('l2_deconv' + str(l2_deconv.get_shape().as_list()))
+	
 	l2 = tf.nn.relu(batch_norm(l2_deconv))
-	#print('l2' + str(l2.get_shape().as_list()))
+	
 	l3_deconv = tf.nn.conv2d_transpose(l2, filter_2 , output_shape = [batch , 14 , 14 , 8] , strides = [1 , 2, 2, 1], padding = 'SAME')
-	#print('l3_deconv', str(l3_deconv.get_shape().as_list()))
+	
 	l3 = tf.nn.relu(batch_norm(l3_deconv))
-	#print('l3' + str(l3.get_shape().as_list()))
+	
 	l4 = tf.nn.conv2d_transpose(l3 , filter_3 , output_shape = [batch , 28 , 28 , 1] , strides = [1 , 2, 2, 1], padding = 'SAME')
-	#print('l4' , str(l4.get_shape().as_list()))
+	
 	return tf.nn.tanh(l4)
 
 G = generator(Z)
 D , D_Logits = discriminator(X)
 D_fake , D_fake_Logits = discriminator(G)
-#print(D_fake_Logits.get_shape().as_list())
+
 
 D_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_Logits, labels=tf.ones_like(D_Logits)))
 D_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake_Logits, labels=tf.zeros_like(D_fake_Logits)))
